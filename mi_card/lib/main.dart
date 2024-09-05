78b79a7f2bfc7b7c3119d0f8a17da7c48a28b194
Asanka Wickramasurendra <asankadilshanme@gmail.com>
2024-09-06 03:24:49 +0530

    Mi_card Started implementing

---
+++ mi_card/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            margin: const EdgeInsets.only(left: 30.0),
+            padding:const EdgeInsets.all(20.00),
+            color: Colors.white,
+            child: const Text('Helloo'),
+          ),
+        ),
+      ),
+    );
+  }
+}
