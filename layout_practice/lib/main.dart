4a1883b8d3c704c23a29c16e41ccf0115f2aef99
Asanka Wickramasurendra <asankadilshanme@gmail.com>
2024-09-16 17:00:14 +0530

    complete lesson4

--- layout_practice/lib/main.dart
+++ layout_practice/lib/main.dart
@@ -39,76 +39,135 @@ class MyApp extends StatelessWidget {
             backgroundColor: Colors.purple,
           ),
           body: Padding(
-            padding: const EdgeInsets.all(20),
+            padding: const EdgeInsets.symmetric(horizontal: 10),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.green,
-                    ),
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.green,
-                    ),
-                  ],
-                ),
                 Container(
-                  width: double.infinity,
-                  height: 100,
-                  color: Colors.green,
-                ),
-                Container(
-                  height: 300,
+                  height: 175,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
                           Container(
-                            width: 100,
-                            height: 100,
-                            color: Colors.blue,
-                          ),
-                          Container(
-                            width: 100,
-                            height: 100,
-                            color: Colors.blue,
-                          ),
+                            height: 175,
+                            width: 175,
+                            color: Colors.green,
+                          )
                         ],
                       ),
                       Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           Container(
-                            width: 100,
-                            height: 50,
-                            color: Colors.purple,
+                            height: 80,
+                            width: 175,
+                            color: Colors.yellow,
                           ),
                           Container(
-                            width: 100,
-                            height: 50,
-                            color: Colors.purple,
-                          ),
-                          Container(
-                            width: 100,
-                            height: 50,
-                            color: Colors.purple,
+                            height: 80,
+                            width: 175,
+                            color: Colors.yellow,
                           ),
                         ],
                       ),
                     ],
                   ),
                 ),
+
+                Container(
+                  width: double.infinity,
+                  height: 100,
+                  color: Colors.purple,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 100,
+                  color: Colors.red,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        Container(
+                          height: 100,
+                          width: 150,
+                          color: Colors.green,
+                        ),
+                        Container(
+                          height: 100,
+                          width: 150,
+                          color: Colors.green,
+                        ),
+                      ],
+                    ),
+                    Container(
+                      height: 200,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            height: 50,
+                            width: 150,
+                            color: Colors.purple,
+                          ),
+                          Container(
+                            height: 50,
+                            width: 150,
+                            color: Colors.purple[800],
+                          ),
+                          Container(
+                            height: 50,
+                            width: 150,
+                            color: Colors.purple,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      height: 75,
+                      width: 75,
+                      decoration: BoxDecoration(
+                        borderRadius:BorderRadius.circular(100),
+                        color: Colors.green,
+                      ),
+                    ),
+                    Container(
+                      height: 75,
+                      width: 75,
+                      decoration: BoxDecoration(
+                        borderRadius:BorderRadius.circular(100),
+                        color: Colors.blue,
+                      ),
+                    ),
+                    Container(
+                      height: 75,
+                      width: 75,
+                      decoration: BoxDecoration(
+                        borderRadius:BorderRadius.circular(100),
+                        color: Colors.yellow,
+                      ),
+                    ),
+                    Container(
+                      height: 75,
+                      width: 75,
+                      decoration: BoxDecoration(
+                        borderRadius:BorderRadius.circular(100),
+                        color: Colors.red,
+                      ),
+                    ),
+                  ],
+                )
               ],
             ),
-          ), 
+          ) 
         ),
       ),
     );
