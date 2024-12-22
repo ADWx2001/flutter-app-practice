a589765ff295d08bed29fa8852c6fbe5b04964ef
Asanka Wickramasurendra <asankadilshanme@gmail.com>
2024-12-22 12:15:23 +0530

    feat : added stripe configurationns

--- stripe_flutter/android/app/src/main/kotlin/com/example/stripe_flutter/MainActivity.kt
+++ stripe_flutter/android/app/src/main/kotlin/com/example/stripe_flutter/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.stripe_flutter
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity(){}
